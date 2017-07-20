f1b6bffa339ade62099e893f0fc3455d6f67c970
apkawa <apkawa@gmail.com>
2017-07-20 18:51:47 +0300

    Fix csrf problem

--- frontend/scripts/components/csrf.js
+++ frontend/scripts/components/csrf.js
@@ -23,11 +23,20 @@ function csrfSafeMethod (method) {
 }
 
 export default function csrfSetup ({cookieName = 'csrftoken', headerName = 'X-CSRFToken'} = {}) {
+  if ($.prototype.__csrf_patched || XMLHttpRequest.prototype._csrf) {
+    return
+  }
   $.ajaxSetup({
     beforeSend: function (xhr, settings) {
+      if ($.prototype.__csrf_patched || XMLHttpRequest.prototype._csrf) {
+        return
+      }
       const csrftoken = getCookie(cookieName)
       if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+        console.log(headerName, csrftoken)
         xhr.setRequestHeader(headerName, csrftoken)
+        $.prototype.__csrf_patched = csrftoken
+        XMLHttpRequest.prototype._csrf = csrftoken
       }
     }
   })
